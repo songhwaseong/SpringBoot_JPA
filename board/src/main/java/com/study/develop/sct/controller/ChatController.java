9da471613d44fcbf9440c0f5df9ed21dace62426
hwaseong <supyoungsun@kakao.com>
2026-03-08 15:03:03 +0900

    스프링부트 웹소켓 설정추가

---
+++ board/src/main/java/com/study/develop/sct/controller/ChatController.java
@@ -0,0 +1,31 @@
+package com.study.develop.sct.controller;
+
+import com.study.develop.sct.dto.ChatMessageDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+    private final SimpMessagingTemplate template;       // 특정 사용자에게 메시지를 보내는데 사용되는 STOMP을 이용한 템플릿입니다.
+
+    @Autowired
+    public ChatController(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
+    /**
+     * Message 엔드포인트로 데이터와 함께 호출을 하면 "/sub/message"를 수신하는 사용자에게 메시지를 전달합니다.
+     *
+     * @param chatMessageDto
+     * @return
+     */
+    @MessageMapping("/messages")
+    public ChatMessageDto send2(@RequestBody ChatMessageDto chatMessageDto) {
+        System.out.println("323423");
+        template.convertAndSend("/sub/message", chatMessageDto.getContent());       // 구독중인 모든 사용자에게 메시지를 전달합니다.
+        return chatMessageDto;
+    }
+}
---
+++ board/src/main/java/com/study/develop/sct/dto/ChatMessageDto.java
@@ -0,0 +1,15 @@
+package com.study.develop.sct.dto;
+
+
+import lombok.Data;
+
+@Data
+public class ChatMessageDto {
+    private String content;
+    private String sender;
+
+    public ChatMessageDto(String content, String sender) {
+        this.content = content;
+        this.sender = sender;
+    }
+}
